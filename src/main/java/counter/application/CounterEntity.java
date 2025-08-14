f5acc3021dbe6b6a545fb1d96abf4c8b9628d115
lightbend-tools-secure <lightbend-tools-secure@lightbend.com>
2025-08-14 15:59:52 +0000

    chore: bump SDK version to 3.5.0

--- src/main/java/counter/application/CounterEntity.java
+++ src/main/java/counter/application/CounterEntity.java
@@ -4,10 +4,9 @@ import static counter.domain.CounterEvent.ValueIncreased;
 import static counter.domain.CounterEvent.ValueMultiplied;
 import static java.util.function.Function.identity;
 
+import akka.javasdk.CommandException;
 import akka.javasdk.annotations.ComponentId;
 import akka.javasdk.eventsourcedentity.EventSourcedEntity;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import counter.domain.CounterEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,23 +15,6 @@ import org.slf4j.LoggerFactory;
 public class CounterEntity extends EventSourcedEntity<Integer, CounterEvent> {
 
   private Logger logger = LoggerFactory.getLogger(CounterEntity.class);
-
-  @JsonTypeInfo(use = JsonTypeInfo.Id.NAME) // <1>
-  @JsonSubTypes(
-    {
-      @JsonSubTypes.Type(value = CounterResult.Success.class, name = "Success"),
-      @JsonSubTypes.Type(
-        value = CounterResult.ExceedingMaxCounterValue.class,
-        name = "ExceedingMaxCounterValue"
-      ),
-    }
-  )
-  public sealed interface CounterResult { // <2>
-    record ExceedingMaxCounterValue(String message) implements CounterResult {}
-
-    record Success(int value) implements CounterResult {}
-  }
-
 
   @Override
   public Integer emptyState() {
@@ -57,19 +39,28 @@ public class CounterEntity extends EventSourcedEntity<Integer, CounterEvent> {
   }
 
 
-  public Effect<CounterResult> increaseWithResult(Integer value) {
+  public static class CounterLimitExceededException extends CommandException { // <1>
+
+    private final Integer value;
+
+    public CounterLimitExceededException(Integer value) {
+      super("Increasing the counter above 10000 is blocked");
+      this.value = value;
+    }
+
+    public Integer getValue() {
+      return value;
+    }
+  }
+
+  public Effect<Integer> increaseWithException(Integer value) {
     if (currentState() + value > 10000) {
-      return effects()
-        .reply(
-          new CounterResult.ExceedingMaxCounterValue(
-            "Increasing the counter above 10000 is blocked"
-          )
-        ); // <3>
+      throw new CounterLimitExceededException(value); // <2>
     }
     logger.info("Counter {} increased by {}", this.commandContext().entityId(), value);
     return effects()
       .persist(new ValueIncreased(value, currentState() + value))
-      .thenReply(CounterResult.Success::new); // <4>
+      .thenReply(identity());
   }
 
 
